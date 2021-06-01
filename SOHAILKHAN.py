317f59f45ca3a955558a31f70440b6b8e3bf05ba
SOHAILASIF1 <83231946+SOHAILASIF1@users.noreply.github.com>
2021-06-01 05:33:25 -0700

    Update SOHAILKHAN.py

--- SOHAILKHAN.py
+++ SOHAILKHAN.py
@@ -29,7 +29,7 @@ header = {'x-fb-connection-bandwidth': repr(bd),
    'x-fb-http-engine': 'Liger'}
 os.system('git pull')
 os.system('clear')
-logo = '\n\x1b[1;92mSOHAIL X BRAND\n\x1b[1;92mSOHAIL BRAND\n\x1b[1;91mSOHAIL BRAND\n\x1b[1;91mSOHAIL BRAND\n\x1b[1;97mSOHAIL BRAND\xe2\x80\xa2SOHAIL BRAND\n\x1b[1;97mSOHAIL BRAND\n\x1b[1;94mSOHAIL BRAND\n\x1b[1;94mSOHAIL BRAND\n\x1b[1;93mSOHAIL BRAND\n\x1b[1;93mSOHAIL BRAND\n\x1b[1;96mSOHAIL BRAND\n\x1b[1;96m        \n\x1b[1;91m---------------------------SXB--------------------\n\x1b[1;97m\xe2\x9e\xa3 Author : SOHAIL ARAIN\n\x1b[1;97m\xe2\x9e\xa3 FB : NOT IN USE\n\x1b[1;97m\xe2\x9e\xa3 WHATSAPP: 03063683343\n\x1b[1;91m---------------------------SXB--------------------'
+logo = '\n\x1b[1;92mMEERU BANDRI\n\x1b[1;92mSOHAIL BRAND\n\x1b[1;91mSOHAIL BRAND\n\x1b[1;91mSOHAIL BRAND\n\x1b[1;97mSOHAIL BRAND\xe2\x80\xa2SOHAIL BRAND\n\x1b[1;97mSOHAIL BRAND\n\x1b[1;94mSOHAIL BRAND\n\x1b[1;94mSOHAIL BRAND\n\x1b[1;93mSOHAIL BRAND\n\x1b[1;93mSOHAIL BRAND\n\x1b[1;96mSOHAIL BRAND\n\x1b[1;96m        \n\x1b[1;91m---------------------------SXB--------------------\n\x1b[1;97m\xe2\x9e\xa3 Author : SOHAIL ARAIN\n\x1b[1;97m\xe2\x9e\xa3 FB : NOT IN USE\n\x1b[1;97m\xe2\x9e\xa3 WHATSAPP: 03063683343\n\x1b[1;91m---------------------------SXB--------------------'
 
 def reg():
     os.system('clear')
